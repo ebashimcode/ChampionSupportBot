b8dbb0c7f4266abc28d6e409f2c39a4910479bbf
nsvk13 <nsvkdev@gmail.com>
2024-05-14 10:05:15 +0300

    chore: мать тереза всех примет в дом

---
+++ src/handlers/handlersHook.js
@@ -0,0 +1,5 @@
+const { startHandler, agentMenuHandler, agentReplyHandler } = require('./');
+
+const startHandler = require('./keyboard/start');
+const agentMenuHandler = require('./agent/agentMenuHandler');
+const agentReplyHandler = require('./agent/agentReplyHandler');
