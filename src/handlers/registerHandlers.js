f955b292fcaba1ed957c00ca921cec1d9ee76fd1
nsvk13 <nsvkdev@gmail.com>
2024-05-21 11:59:03 +0300

    chore: update handlers

--- src/handlers/registerHandlers.js
+++ src/handlers/registerHandlers.js
@@ -1,5 +1,9 @@
 const startHandler = require('./commands/startHandler');
+const supportHandler = require('./commands/supportHandler');
+const replyHandler = require('./agents/replyHandler');
 
 module.exports = (bot) => {
   startHandler(bot);
+  bot.command('support', supportHandler(bot));
+  bot.use(replyHandler);
 };
