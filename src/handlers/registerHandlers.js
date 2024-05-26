f6e7c12369019c1742db29d63f309ac0aa68b303
nsvk13 <nsvkdev@gmail.com>
2024-05-26 17:26:30 +0300

    added new handler and stages

--- src/handlers/registerHandlers.js
+++ src/handlers/registerHandlers.js
@@ -1,11 +1,18 @@
 const startHandler = require('./commands/startHandler');
 const supportHandler = require('./commands/supportHandler');
 const replyHandler = require('./agents/replyHandler');
-const faqHandler = require('./commands/faqHandler');
+const { codeOperators } = require('../const');
 
 module.exports = (bot) => {
   startHandler(bot);
   bot.hears(/.*/, replyHandler);
-  bot.action(/faq_.*/, faqHandler);
   bot.command('support', supportHandler(bot));
+
+  bot.command('admin', (ctx) => {
+    if (codeOperators.includes(ctx.from.id)) {
+      ctx.scene.enter('ADMIN_PANEL_SCENE');
+    } else {
+      ctx.reply('У вас нет доступа к этой команде.');
+    }
+  });
 };
