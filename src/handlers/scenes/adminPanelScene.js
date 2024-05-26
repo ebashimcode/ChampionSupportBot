40b9b94019bcda97f5cb9edec4df5ba7c44d0cb2
nsvk13 <nsvkdev@gmail.com>
2024-05-26 17:43:27 +0300

    chore: new scenes

---
+++ src/handlers/scenes/adminPanelScene.js
@@ -0,0 +1,32 @@
+const { Scenes, Markup } = require('telegraf');
+const { getUserCount, getAllUsers } = require('../../data/DB');
+const { adminMenu, botMenu } = require('../../const');
+
+const adminPanelScene = new Scenes.BaseScene('ADMIN_PANEL_SCENE');
+
+adminPanelScene.enter(async (ctx) => {
+    await ctx.reply(
+        'Добро пожаловать в админ-панель. Выберите действие:',
+        Markup.keyboard(adminMenu).resize().oneTime()
+    );
+});
+
+adminPanelScene.hears('Статистика 📊', async (ctx) => {
+    const userCount = await getUserCount();
+    await ctx.reply(`Количество пользователей: ${userCount}`);
+});
+
+adminPanelScene.hears('Создать рассылку 📢', async (ctx) => {
+    await ctx.reply('Введите сообщение для рассылки:');
+    ctx.scene.enter('BROADCAST_SCENE');
+});
+
+adminPanelScene.hears('Назад 🔙', async (ctx) => {
+    await ctx.reply(
+        'Здравствуйте! Вы обратились в тех.поддержку проекта Champion Casino.\nПо какому вопросу вы обращаетесь?',
+        Markup.keyboard(botMenu).resize().oneTime()
+    );
+    ctx.scene.leave();
+});
+
+module.exports = adminPanelScene;
