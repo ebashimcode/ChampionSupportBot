c2a5803a8e31e5da9870dd2962dd66dc57db1dc7
nsvk13 <nsvkdev@gmail.com>
2024-05-26 17:44:54 +0300

    feat: damn idk where changes

--- src/handlers/scenes/userAdminChat.js
+++ src/handlers/scenes/userAdminChat.js
@@ -1,18 +1,18 @@
-const { Scenes } = require('telegraf');
-const { codeOperators } = require('../../const');
+const { Scenes, Markup } = require('telegraf');
+const { codeOperators, botMenu } = require('../../const');
 
 const userAdminChatScene = new Scenes.BaseScene('USER_ADMIN_CHAT_SCENE');
 
 userAdminChatScene.enter(async (ctx) => {
   await ctx.reply(
-    '✉️ Напишите своё сообщение для технической поддержки.\nДля выхода из чата введите /quit'
+      '✉️ Напишите своё сообщение для технической поддержки.\nДля выхода из чата введите /quit'
   );
 });
 
 userAdminChatScene.hears(/\/quit/, async (ctx) => {
   await ctx.reply(
-    'Здравствуйте! Вы обратились в тех.поддержку проекта Champion Casino.\nПо какому вопросу вы обращаетесь?',
-    Markup.keyboard(botMenu).resize().oneTime()
+      'Здравствуйте! Вы обратились в тех.поддержку проекта Champion Casino.\nПо какому вопросу вы обращаетесь?',
+      Markup.keyboard(botMenu).resize().oneTime()
   );
   ctx.scene.leave();
 });
@@ -24,8 +24,8 @@ userAdminChatScene.on('message', async (ctx) => {
 
   for (const operatorId of codeOperators) {
     await ctx.telegram.sendMessage(
-      operatorId,
-      `Обращение по категории: ${ctx.session.supportCategory}\n\nСообщение от ${username}:\n\n${message}\n\n#id${userId}`
+        operatorId,
+        `Обращение по категории: ${ctx.session.supportCategory}\n\nСообщение от ${username}:\n\n${message}\n\n#id${userId}`
     );
   }
 });
