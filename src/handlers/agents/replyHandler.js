e7b31705a2369fb187d3d28b21ca4917a7d9b72b
nsvk13 <nsvkdev@gmail.com>
2024-05-29 00:35:28 +0300

    refactor? no, that shit.

--- src/handlers/agents/replyHandler.js
+++ src/handlers/agents/replyHandler.js
@@ -1,4 +1,4 @@
-const { codeOperators } = require('../../const');
+const { codeOperators } = require('../../data/const');
 
 module.exports = async function replyHandler(ctx, next) {
     if (!codeOperators.includes(ctx.from.id)) {
@@ -6,8 +6,17 @@ module.exports = async function replyHandler(ctx, next) {
     }
 
     const messageText = ctx.message.text;
-
     const destId = ctx.session.replyId;
 
-    await ctx.telegram.sendMessage(destId, `Сообщение от оператора:\n\n${ctx.message.text}`);
+    if (!destId) {
+        console.error('replyId is not set in session');
+        return ctx.reply('Произошла ошибка: не задан ID получателя.');
+    }
+
+    try {
+        await ctx.telegram.sendMessage(destId, `Сообщение от оператора:\n\n${messageText}`);
+    } catch (error) {
+        console.error('Error sending message:', error);
+        ctx.reply('Произошла ошибка при отправке сообщения.');
+    }
 };
